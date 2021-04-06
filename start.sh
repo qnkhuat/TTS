0d7935a5836eb3e1f12ba69a985c22e5b562f7c1
Ngoc Khuat <qn.khuat@gmail.com>
2021-04-06 08:32:43 +0000

    add start

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
+# Load datset
 mkdir datasets
 echo "Download dataset"
 curl https://data.keithito.com/data/speech/LJSpeech-1.1.tar.bz2 -o datasets/LJSpeech-1.1.tar.bz2
+cd datasets/
+tar -xf LJSpeech-1.1.tar.bz2
+
+# Install dependencies
+cd .. 
+pip3 install -e .
---
+++ train.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES="0" python3 TTS/bin/train_*.py --config_path TTS/*/configs/config.json
